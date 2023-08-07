2fd795d49b796a92ece0bd51509d320492a9218b
AmanSingh494 <amanforwork1@gmail.com>
2023-08-07 20:27:57 +0530

    Updated app,js and index.html

--- app.js
+++ app.js
@@ -4,37 +4,56 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-const date = document.querySelector("#date")
+const date = document.querySelector('#date')
 date.innerHTML = new Date().getFullYear()
 // ********** close links ************
 const navToggle = document.querySelector('.nav-toggle')
 const linksContainer = document.querySelector('.links-container')
 const links = document.querySelector('.links')
-navToggle.addEventListener("click", () => {
+navToggle.addEventListener('click', () => {
   // linksContainer.classList.toggle('show-links')
   const containerHeight = linksContainer.getBoundingClientRect().height
   const linksHeight = links.getBoundingClientRect().height
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`
-  }else{
+  } else {
     linksContainer.style.height = 0
   }
-
-  
 })
 // ********** fixed navbar ************
 const navbar = document.querySelector('#nav')
 const topLink = document.querySelector('.top-link')
 const home = document.querySelector('#home')
-window.addEventListener("scroll", ()=>{
-  const scrollHeight = window.pageYOffset
+window.addEventListener('scroll', () => {
+  const scrollHeight = document.documentElement.scrollTop
   const navbarHeight = navbar.getBoundingClientRect().height
-const homeHeight = home.getBoundingClientRect().height
-if(scrollHeight > navbarHeight){
-  navbar.classList.add('fixed-nav')
-}else {
-  navbar.classList.remove('fixed-nav')
-}
+  const homeHeight = home.getBoundingClientRect().height
+  if (scrollHeight > navbarHeight) {
+    navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
+  if (scrollHeight > homeHeight) {
+    topLink.classList.add('show-link')
+  } else {
+    topLink.classList.remove('show-link')
+  }
 })
 // ********** smooth scroll ************
 // select links
+const scrollLink = document.querySelectorAll('.scroll-link')
+scrollLink.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    // navigating to a specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+    // calculating heights
+    const navbarHeight = navbar.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClient().height
+    const fixedNav = navbar.classList.contains('fixed-nav')
+    const scrollY = element.offsetTop
+    window.scrollTo(0, scrollY)
+    linksContainer.style.height = 0
+  })
+})
